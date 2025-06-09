7cd4696bf688a59dd96c8815b945767563104aa3
Nico Hidalgo <rhidalgop@est.ups.edu.ec>
2025-06-09 16:43:07 -0500

    Variables, consola y arreglos

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 function MostrarMensaje(){
     alert("Hola mundo:");
 }
+function ImprimirVariables(){
+    let nombre = "Nico";
+    let edad = 20;
+    console.log("Nombre: ",nombre);
+    console.log("Edad: ",edad);
+    console.error("Error: shunsho");
+    console.warn("Advertencia: XXXX");
+}
+function Arreglos(){
+    let frutas = ['manzana','pera','mango'];
+    for (let f of frutas){
+        console.log(f);
+    }
+}
